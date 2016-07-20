46eaf3b7d11ee76365833102940a69520a1ec8d7
dillon-mckee <dillon.mckee@gmail.com>
2016-07-20 17:07:47 -0400

    first commit

--- js/app.js
+++ js/app.js
@@ -1,6 +1,53 @@
-
 $(document).ready(function(){
-	
+  $('form').submit(function(event) {
+    event.preventDefault();
+    var gameGuess = $('#userGuess').val();
+    fizzBuzz(gameGuess);
+  });
+
+    // function testFunction(x, y) {
+    //  return x + y;
+    // }
+    // console.log(testFunction(5, 10));
+
+  function fizzBuzz(max) {
+    console.log(max);
+    for (var i = 0; i <= max; i++) {
+      if (i % 3 === 0 && i % 5 === 0) {
+        console.log("fizz buzz");
+      }
+      else if (i % 3 === 0) {
+        console.log("fizz");
+      }
+      else if (i % 5 === 0) {
+        console.log("buzz");
+      }
+      else {
+        console.log(i);
+      }
+    }
+  }
+
+
+    //var gameGuess = $('#userGuess').val();
+    //document.log(gameGuess);
+    //});
+
+  // onLoad();
+  // function onLoad() {
+  //   newGame();
+
+  // }
+
+  // function newGame() {
+
+  // }
+
+  // function generateNumber() {
+  //   var secretNumber = Math.floor((Math.random() * 100) + 1);
+  // }
+
+
 	/*--- Display information modal box ---*/
   	$(".what").click(function(){
     	$(".overlay").fadeIn(1000);
@@ -11,6 +58,8 @@ $(document).ready(function(){
   	$("a.close").click(function(){
   		$(".overlay").fadeOut(1000);
   	});
+
+
 
 });
 
